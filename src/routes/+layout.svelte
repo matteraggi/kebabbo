b9c8dc8f33ea7e8fd27cee90f1b90f86724d1b0a
Matteo Raggi <matteo.raggi04@gmail.com>
2024-03-14 09:23:23 +0100

    feat: navbar closing automatically

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	import "../app.css";
+
+	if (typeof document !== 'undefined') {
+    const toggleMenu = document.getElementById('toggleMenu') as HTMLInputElement | null;
+    const navbarSticky = document.getElementById('navbar-sticky');
+
+    if (toggleMenu) {
+        document.querySelectorAll('#navbar-sticky a').forEach(anchor => {
+            anchor.addEventListener('click', () => {
+                toggleMenu.checked = false;
+            });
+        });
+    }
+}
+
+
 </script>
 
 <nav class="bg-[#c71010] rounded-b-xl fixed w-full z-20 top-0 start-0">
@@ -9,32 +24,29 @@
 				<img src="./kebabbo-scritta-navbar.png" class="h-10" alt="Kebbabo Scritta">
 			</a>
 		</div>
-	<div class="hidden lg:flex md:order-2 md:space-x-0 rtl:space-x-reverse w-[20%] justify-end">
-		<img src="./kebabbo-logo-navbar.png" class="h-10" alt="Kebbabo Logo">	
-	</div>
-	<input type="checkbox" id="toggleMenu" class="hidden">
-    <label for="toggleMenu" class="md:hidden">
-		<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-			<path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-		  </svg>
+		<div class="hidden lg:flex md:order-2 md:space-x-0 rtl:space-x-reverse w-[20%] justify-end">
+			<img src="./kebabbo-logo-navbar.png" class="h-10" alt="Kebbabo Logo">	
+		</div>
+		<input type="checkbox" id="toggleMenu" class="hidden">
+		<label for="toggleMenu" class="md:hidden">
+			<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+				<path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+			  </svg>
 		</label> 
-	<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-	  <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 ">
-		<li>
-			<a href="/" class="block py-2 px-3 text-white rounded md:p-0 text-xl" aria-current="page">Maps</a>
-		</li>
-		<li>
-			<a href="/charts" class="block py-2 px-3 text-white rounded md:p-0 text-xl">Charts</a>
-		</li>
-		<li>
-			<a href="/about" class="block py-2 px-3 text-white rounded md:p-0 text-xl">About</a>
-		</li>
-	  </ul>
+		<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+			<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 ">
+				<li>
+					<a href="/" class="block py-2 px-3 text-white rounded md:p-0 text-xl" aria-current="page">Maps</a>
+				</li>
+				<li>
+					<a href="/charts" class="block py-2 px-3 text-white rounded md:p-0 text-xl">Charts</a>
+				</li>
+				<li>
+					<a href="/about" class="block py-2 px-3 text-white rounded md:p-0 text-xl">About</a>
+				</li>
+			</ul>
+		</div>
 	</div>
-	</div>
-  </nav>
-  
+</nav>
 
-
-<slot>
-</slot>
+<slot></slot>
