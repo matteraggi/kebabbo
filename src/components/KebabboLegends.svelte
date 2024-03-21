62ebe5352a46b943daaa7eb64f157da0043a6b92
Matteo Raggi <matteo.raggi04@gmail.com>
2024-03-21 15:39:31 +0100

    feat: new sorting params

--- src/components/KebabboLegends.svelte
+++ src/components/KebabboLegends.svelte
@@ -13,6 +13,26 @@
                     currentOrder = "rating";
                     dbArray = dbArray.sort((a, b) => order ? a.rating - b.rating : b.rating - a.rating);
                     break;
+                case "quality":
+                    currentOrder = "name";
+                    dbArray = dbArray.sort((a, b) => order ? a.quality - b.quality : b.quality - a.quality);
+                    break;
+                case "price":
+                    currentOrder = "price";
+                    dbArray = dbArray.sort((a, b) => order ? a.price - b.price : b.price - a.price);
+                    break;
+                case "dimension":
+                    currentOrder = "dimension";
+                    dbArray = dbArray.sort((a, b) => order ? a.dimension - b.dimension : b.dimension - a.dimension);
+                    break;
+                case "fun":
+                    currentOrder = "fun";
+                    dbArray = dbArray.sort((a, b) => order ? a.fun - b.fun : b.fun - a.fun);
+                    break;
+                case "menu":
+                    currentOrder = "menu";
+                    dbArray = dbArray.sort((a, b) => order ? a.menu - b.menu : b.menu - a.menu);
+                    break;
                 case "distance":
                     currentOrder = "distance";
                     break;
@@ -42,15 +62,21 @@
 <div class="flex-col lg:flex lg:flex-row justify-between mb-3 align-middle">
     <div class="flex-col lg:flex lg:flex-row gap-3 items-center">
         <p class="text-xl text-black font-bold h-fit mb-3">Sort by:</p>
-        <div class="flex gap-3 mb-3">
-            <div>
+        <div class="flex-col lg:flex lg:flex-row gap-3 mb-3">
+            <div class="flex gap-3 mb-3 lg:mb-0">
                 <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("rating")} aria-expanded={order}>Rating</button>
+            
+                <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("price")} aria-expanded={order}>Price</button>
+                
+                <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("dimension")} aria-expanded={order}>Dimension</button>
+            </div>
+            <div class="flex gap-3 mb-3 lg:mb-0">
+                <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("fun")} aria-expanded={order}>Fun</button>
+                <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("menu")} aria-expanded={order}>Menu</button>
+                <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("name")} aria-expanded={order}>Name</button>
             </div>
             <div>
                 <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("distance")} aria-expanded={order}>Distance</button>
-            </div>
-            <div>
-                <button class="bg-[#c71010] px-3 py-1 text-white rounded-3xl" on:click={() => setOrder("name")} aria-expanded={order}>Name</button>
             </div>
         </div>
     </div>
