016143f7e4567d3bf387eaa7c4bb06a5f3c2eaa7
Matteo Raggi <matteo.raggi04@gmail.com>
2024-03-21 16:42:46 +0100

    .

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,7 @@ import AboutKebabboInfo from "../../components/AboutKebabboInfo.svelte";
 
 </script>
 <div class="flex flex-col align-middle items-center bg-[#ffba1c] min-h-screen pt-24 w-full">
-    <div class="w-[60%] flex flex-col mt-6">
+    <div class="w-[80%] lg:w-[60%] flex flex-col mt-6">
         <h1 class="text-4xl font-bold">La tua soluzione per il pranzo universitario</h1>
         <div class="flex flex-col items-center">
         <AboutKebabboInfo />
