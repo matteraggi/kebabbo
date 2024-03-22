3b460fc0786a80038e427b5447ba62d5f9f884a7
Matteo Raggi <matteo.raggi04@gmail.com>
2024-03-22 17:41:41 +0100

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,18 +15,6 @@
 		}
 	}
 
-	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', () => {
-			navigator.serviceWorker
-				.register('./../service-worker.js')
-				.then((registration) => {
-					console.log('Service Worker registered with scope:', registration.scope);
-				})
-				.catch((error) => {
-					console.error('Service Worker registration failed:', error);
-				});
-		});
-	}
 </script>
 
 <nav class="bg-[#c71010] rounded-b-xl fixed w-full z-20 top-0 start-0">
