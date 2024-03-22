f94e0bd7169be1f0367d03df5b2cf92cfdd23e3f
Matteo Raggi <matteo.raggi04@gmail.com>
2024-03-22 08:11:30 +0100

    feat: map mobile width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -63,7 +63,7 @@ const centerOnUser = () => {
   };
   </script>
 <div class="flex flex-col align-middle items-center bg-[#ffba1c] h-screen pt-24 w-full">
-  <div class="w-[60%] lg:w-[60%] h-full">
+  <div class="w-[80%] lg:w-[60%] h-full">
     <h1 class="text-3xl">Kebabbo Maps</h1> <button on:click={centerOnUser}>Center on Me</button>
     <div  id="map-container" class="my-7 align-middle items-center "/>
     </div>
